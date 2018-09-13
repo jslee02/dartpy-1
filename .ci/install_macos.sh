4c391e293c9f513885f9f4797d9a4819fbebb4de
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-09-12 23:54:43 -0700

    Update install_macos.sh

--- .ci/install_macos.sh
+++ .ci/install_macos.sh
@@ -2,9 +2,12 @@
 set -x
 # e option is disabled for now
 
+brew tap personalrobotics/tap
+
 brew update > /dev/null
 
 # TODO: the list isn't complete
 brew install dartsim
 brew install boost
 brew install boost-python3
+brew install chimera
